a395f1b17d059324a97f455ff8880c6e0929c581
Chacko <chackop@gmail.com>
2020-09-18 11:21:18 +0100

    obj iterate

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const lakeList = ['Echo Lake', 'Maud Lake', 'Cascade Lake'];
+const lakeList = [
+  { id: '1', name: 'Echo', trailhead: 'Echo' },
+  { id: '2', name: 'Maud', trailhead: 'Wrights' },
+  { id: '3', name: 'Velma', trailhead: 'Bayview' },
+];
 
 function App({ lakes }) {
   return (
-    <ul>
+    <div>
       {lakes.map((lake) => (
-        <li>{lake}</li>
+        <div>
+          <h2>{lake.name}</h2>
+          <p>Accessed by: {lake.trailhead}</p>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
 
