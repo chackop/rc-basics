38cb43be71c1e2aa76e5c38bdf2148451ebdafad
Chacko <chackop@gmail.com>
2020-09-18 11:14:52 +0100

    props

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Hello({ library, message, number }) {
+  return (
+    <div>
+      <h1>Welcome to {library}!</h1>
+      <p>{message}</p>
+      <p>{number} Props Total</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <ul>
-    <li>Hot Dogs</li>
-    <li>Hamburgers</li>
-    <li>Pizza</li>
-    <li>Sushi</li>
-  </ul>,
+  <Hello library="React" message="add dynamic data!" number={3} />,
   document.getElementById('root')
 );
