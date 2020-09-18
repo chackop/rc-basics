34ac4cd2185794057b9fd34724ac70b38b5128bc
Chacko <chackop@gmail.com>
2020-09-18 11:12:32 +0100

    jsx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  React.createElement('h1', { style: { color: 'blue' } }, 'Hello!'),
+  <ul>
+    <li>Hot Dogs</li>
+    <li>Hamburgers</li>
+    <li>Pizza</li>
+    <li>Sushi</li>
+  </ul>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
